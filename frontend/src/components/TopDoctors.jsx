eefc055136d9176285ec716c71588d8dd999da3f
AnikaTahshin <anikatahsinmeem@gmail.com>
2026-04-26 18:05:45 +0600

    banner section done

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import { doctors } from "../assets/assets_frontend/assets"
+import { AppContext } from "../context/AppContext";
+import { useContext } from "react";
+// import { doctors } from "../assets/assets_frontend/assets"
 
 const TopDoctors = () => {
 
    const navigate = useNavigate(); 
+   const {doctors} = useContext(AppContext)
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
         <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -23,7 +26,7 @@ const TopDoctors = () => {
                 </div>
             ))}
         </div>
-                    <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10" type="button">More</button>
+                    <button onClick={() => {navigate('/doctors'); scrollTo(0,0)}} className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10" type="button">More</button>
 
     </div>
   )
