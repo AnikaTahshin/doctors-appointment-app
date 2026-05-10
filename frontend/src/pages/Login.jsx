a07c9931bd40b87a4d129c31c407515b994f4266
AnikaTahshin <anikatahsinmeem@gmail.com>
2026-05-10 22:19:11 +0600

    login and sign up ui done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [state, setState] = useState('Sign Up')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('');
+
+const onSubmitHandler = async(ev) => {
+  ev.preventDefault();
+
+
+
+}
+
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-85 sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointment</p>
+
+        {
+          state === 'Sign Up' && <div className="w-full">
+          <p>Name</p>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" required type="text" onChange={(e) => setName(e.target.value)} value={name} />
+        </div>
+        }
+
+
+        <div className="w-full">
+          <p>Email</p>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" required  type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        </div>
+
+
+        <div className="w-full">
+          <p>Password</p>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" required type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+        </div>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base" type="submit">{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === 'Sign Up' ? <p>Already have an account? <span className="text-primary cursor-pointer" onClick={() => setState('Login')}>Login</span></p> : <p>Don't have an account? <span className="text-primary cursor-pointer" onClick={() => setState('Sign Up')}>Sign Up</span></p>
+        }
+      </div>
+
+    </form>
+  );
 };
 
 export default Login;
